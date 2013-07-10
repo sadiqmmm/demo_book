aac085c7376544a834201cde133380631f61ed13
mohammed sadiq <sadiqmmm@gmail.com>
2013-07-10 23:55:41 +0530

    Fixed profile test

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+
 	test "a user should have a first name" do 
 		user = User.new
 		assert !user.save
@@ -26,17 +27,24 @@ class UserTest < ActiveSupport::TestCase
 
 		assert !user.save
 		assert !user.errors[:profile_name].empty?
-
 	end
 
 	test "a user should have a profile name without spaces" do 
-		user = User.new
+		user = User.new(first_name: 'mohammed', last_name: 'sadiq', email: 'sadiq@sss.com')
+		user.password = user.password_confirmation = "password"
+		
 		user.profile_name = "My profile with spaces"
 
 		assert !user.save
 		assert !user.errors[:profile_name].empty?
 		assert user.errors[:profile_name].include?("Must be formatted correctly")
+	end
 
+	test "a user can have a correctly formatted profile name" do
+
+		user = User.new(first_name: 'mohammed', last_name: 'sadiq', email: 'sadiq@sss.com')
+		user.password = user.password_confirmation = "password"
+		user.profile_name ='sadiq'
+		assert user.valid?
 	end
 end
-1
